e8561ea67eb81f07e5f50fbcf510c6e26d691e0d
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 14:58:40 +0900

    add nest describe

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -15,19 +15,26 @@ RSpec.describe Hello do
   #it do ... endの中のエクスペクテーションが全てパスすれば、そのexampleはパスしたことになります。
   #toとeqはマッチャと呼ばれる機能。
 
-  it "1 + 1 は 2 になること" do
-    expect(1 + 1).to eq 2
-  end
+  describe '四則演算' do
+    describe '足し算' do
+      it "1 + 1 は 2 になること" do
+        expect(1 + 1).to eq 2
+      end
+   end
+   describe '引き算' do
+      it "10 - 1 は 9 になること" do
+        expect(10 - 1).to eq 9
+      end
+    end
 
-  it "10 - 1 は 9 になること" do
-    expect(10 - 1).to eq 9
-  end
-
-  it "全部できること" do
-    expect(1 + 2).to eq 3
-    expect(10 - 1).to eq 9
-    expect(4 * 8).to eq 32
-    expect(40 / 5).to eq 8
+    it "全部できること" do
+      expect(1 + 2).to eq 3
+      expect(10 - 1).to eq 9
+      expect(4 * 8).to eq 32
+      expect(40 / 5).to eq 8
+    end
   end
   #このように1つのexampleの中に複数のエクスペクテーションを書くのも良いが、テストが失敗した時にその先のエクスペクテーションが通過するのかしないのか判断ができなくなってしまう。
+
+
 end
