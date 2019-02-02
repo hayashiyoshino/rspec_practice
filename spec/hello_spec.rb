d31dff8d03a5ada822e0d9f606b622bf641ebda4
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 12:54:02 +0900

    change require way

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,8 +1,9 @@
-require_relative '../lib/hello'
+#require_relative '../lib/hello'
 #RailsではなくRubyファイルの時はrequireする。
 #Railsは自動でrequireされている。
 #require_relativeを使うと相対パスで指定できる。
 
+require 'spec_helper'
 
 RSpec.describe Hello do
   it "message return hello" do
