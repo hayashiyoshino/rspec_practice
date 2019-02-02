fcec2915e6cd22fe53a87e741422de825572d30e
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 12:47:21 +0900

    add comment of require

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,4 +1,8 @@
 require_relative '../lib/hello'
+#RailsではなくRubyファイルの時はrequireする。
+#Railsは自動でrequireされている。
+#require_relativeを使うと相対パスで指定できる。
+
 
 RSpec.describe Hello do
   it "message return hello" do
