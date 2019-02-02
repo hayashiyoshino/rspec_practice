ff928ad747b9d037cf783b6ef64003f89eb3e0da
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 16:42:53 +0900

    change before do @params to let(:params)

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,30 +2,33 @@ require 'spec_helper'
 
 RSpec.describe User do
   #describeにはdescribe User のように文字列ではなくクラスも渡せる。
-  before do
-    @params = { name: 'たろう' }
-  end
+  # before do
+  #   @params = { name: 'たろう' }
+  # end
+
 
   describe '#greet' do
     #「インスタンスメソッドのgreetメソッドをテストしますよ」という意味でdescribe '#greet' のように書くこともある。
+    let(:params) { {name: 'たろう'} }
+
     context '12歳以下の場合' do
       #describe以外にもcontextという機能でテストをグループ化することもできます。
       #contextは条件を分けたりする時に使うことが多いです。
       before do
-        @params.merge!(age: 12)
+        params.merge!(age: 12)
       end
       it '「〜だよ。」で答えること' do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.greet).to eq '私はたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
       before do
-        @params.merge!(age: 13)
+        params.merge!(age: 13)
       end
       it '「〜です。」で答えること' do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.greet).to eq '私はたろうです。'
       end
     end
