db9fd0b312f0a84edd2a4a6fc080a76115dc771c
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 14:45:19 +0900

    add example 10-1は9になること

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -10,7 +10,16 @@ RSpec.describe Hello do
     expect(Hello.new.message).to eq "hello"
   end
 
+  #describeはテストのグループ化を宣言。翻訳すると「〜をのべる、記述する、説明する」
+  #itはテストをexampleという単位にまとめる役割をします。
+  #it do ... endの中のエクスペクテーションが全てパスすれば、そのexampleはパスしたことになります。
+  #toとeqはマッチャと呼ばれる機能。
+
   it "1 + 1 は 2 になること" do
     expect(1 + 1).to eq 2
+  end
+
+  it "10 - 1 は 9 になること" do
+    expect(10 - 1).to eq 9
   end
 end
