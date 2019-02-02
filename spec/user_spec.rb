86190a6fe27b09062285ce4a9bc32aa536ec6c87
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-02 15:26:11 +0900

    use context

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe User do
   #describeにはdescribe User のように文字列ではなくクラスも渡せる。
   describe '#greet' do
     #「インスタンスメソッドのgreetメソッドをテストしますよ」という意味でdescribe '#greet' のように書くこともある。
-    it '12歳以下の場合「〜だよ。」で答えること' do
-      user = User.new(name: 'たろう', age: 12)
-      expect(user.greet).to eq '私はたろうだよ。'
+    context '12歳以下の場合' do
+      it '「〜だよ。」で答えること' do
+        user = User.new(name: 'たろう', age: 12)
+        expect(user.greet).to eq '私はたろうだよ。'
+      end
     end
 
-    it '13歳以上の場合「〜です。」で答えること' do
-      user = User.new(name: 'たろう', age: 13)
-      expect(user.greet).to eq '私はたろうです。'
+    context '13歳以上の場合' do
+      it '「〜です。」で答えること' do
+        user = User.new(name: 'たろう', age: 13)
+        expect(user.greet).to eq '私はたろうです。'
+      end
     end
   end
 
