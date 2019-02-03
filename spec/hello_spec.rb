a8a9f48b0d84e0e54d5bca9e38e96cf45a9ebd6b
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-03 20:32:02 +0900

    fix indent and remove comments

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe Hello do
       it "1 + 1 は 2 になること" do
         expect(1 + 1).to eq 2
       end
-   end
-   describe '引き算' do
+    end
+    describe '引き算' do
       it "10 - 1 は 9 になること" do
         expect(10 - 1).to eq 9
       end
